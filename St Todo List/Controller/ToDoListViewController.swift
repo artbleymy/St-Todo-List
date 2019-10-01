44725c443c839d1d8cc0ea7c822dbb61170633bb
Stanislav Kozlov <bleymy@mail.ru>
2019-10-01 11:16:43 +0300

    Convert to MVC design pattern

--- St Todo List/Controller/ToDoListViewController.swift
+++ St Todo List/Controller/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var itemArray = ["Milk", "Potatoes", "Water"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
@@ -24,12 +24,13 @@ class ToDoListViewController: UIViewController, UITableViewDelegate, UITableView
         let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
             //what will happen
-            guard let newItem = textField.text else { return }
-            if newItem.count > 0 {
+            guard let newItemName = textField.text else { return }
+            if newItemName.count > 0 {
+                let newItem = Item(name: newItemName, checked: false)
                 self.itemArray.append(newItem)
-                
-                self.defaults.set(self.itemArray, forKey: "TodoListArray")
-                
+
+//                self.defaults.set(self.itemArray, forKey: "TodoListArray")
+
                 self.todoTable.reloadData()
             }
         }
@@ -43,17 +44,18 @@ class ToDoListViewController: UIViewController, UITableViewDelegate, UITableView
         present(alert, animated: true, completion: nil)
         
     }
-    
+
     
 //MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         todoTable.delegate = self
         todoTable.dataSource = self
         
+        fillArray()
         //First - use user defaults and load data from them
-        loadDataFromUserDefaults()
+//        loadDataFromUserDefaults()
     }
 //MARK: - tableView DataSsource methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -62,22 +64,53 @@ class ToDoListViewController: UIViewController, UITableViewDelegate, UITableView
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.accessoryType =  item.checked ? .checkmark : .none
         return cell
     }
     
   //MARK: - tableView Delegate methods
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
-        tableView.cellForRow(at: indexPath)?.accessoryType = (tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.none) ? .checkmark : .none
+        itemArray[indexPath.row].checked = !itemArray[indexPath.row].checked
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
     
     //MARK: - Load data from user defaults
     private func loadDataFromUserDefaults(){
-        guard let loadedData = defaults.array(forKey: "TodoListArray") as? [String] else { return }
-        itemArray = loadedData
+//        guard let loadedData = defaults.array(forKey: "TodoListArray") as? [String] else { return }
+//        itemArray = loadedData
+    }
+    
+    private func addItem(name: String){
+        let newItem = Item(name: name)
+        itemArray.append(newItem)
+    }
+    
+    //MARK: - Default initializations for test
+    private func fillArray(){
+        addItem(name: "Milk")
+        addItem(name: "Water")
+        addItem(name: "Cola")
+        addItem(name: "Meat")
+        addItem(name: "Orange")
+        addItem(name: "Cucumber")
+        addItem(name: "Candies")
+        addItem(name: "Juice")
+        addItem(name: "Potato")
+        addItem(name: "Tomato")
+        addItem(name: "Bread")
+        addItem(name: "Eggs")
+        addItem(name: "Apples")
+        addItem(name: "1")
+        addItem(name: "2")
+        addItem(name: "3")
+        addItem(name: "4")
+        addItem(name: "5")
+
     }
 }
 
---
+++ St Todo List/Model/Item.swift
@@ -0,0 +1,23 @@
+//
+//  Item.swift
+//  St Todo List
+//
+//  Created by Stanislav on 01/10/2019.
+//  Copyright © 2019 Stanislav Kozlov. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    let name: String 
+    var checked: Bool = false
+    
+    init(name: String, checked: Bool) {
+        self.name = name
+        self.checked = checked
+        
+    }
+    init(name: String) {
+        self.name = name
+    }
+}
